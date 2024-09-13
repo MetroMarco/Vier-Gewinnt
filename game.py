1afc4fcb4c4edb0cd11eb7511a28255b44850e05
MetroMarco <gianmarco.wolf@metro.digital>
2024-09-13 16:01:33 +0200

    stones get constant names

--- game.py
+++ game.py
@@ -1,14 +1,19 @@
+
+STONE_1 = "0"
+STONE_2 = "@"
+
 # Willkommensbildschirm
 print("Herzlich Willkommen zu Vier Gewinnt")
 
 # Spieler werden gebeten ihre Namen einzugeben
 player_name_1 = input("Spieler 1: Bitte geben Sie ihren Namen ein: ")
-print(player_name_1 + " spielt mit O")
+print(player_name_1 + " spielt mit: " + STONE_1)
 player_name_2 = input("Spieler 2:Bitte geben Sie ihren Namen ein: ")
-print(player_name_2 + " spielt mit @")
+print(player_name_2 + " spielt mit: " + STONE_2)
 
 print("Bitte lege dinen Stein in eine der Spalten 1, 2, 3, 4, 5, 6 oder 7")
 # Spieler wird aufgefordert den Stein in Spalte 1 - 7 zu legen
+
 
 turn_1 = int(input(player_name_1 + " ist am Zug"))
 if 1 <= turn_1 <= 7:
