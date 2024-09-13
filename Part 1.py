45984805824e7fe6d44c373a699a07108bcf7d49
MetroMarco <gianmarco.wolf@metro.digital>
2024-09-13 15:36:50 +0200

    rename variable names

--- Part 1.py
+++ Part 1.py
@@ -2,24 +2,24 @@
 print("Herzlich Willkommen zu Vier Gewinnt")
 
 # Spieler werden gebeten ihre Namen einzugeben
-Spielername1 = input("Spieler 1: Bitte geben Sie ihren Namen ein: ")
-print(Spielername1 + " spielt mit O")
-Spielername2 = input("Spieler 2:Bitte geben Sie ihren Namen ein: ")
-print(Spielername2 + " spielt mit @")
+player_name_1 = input("Spieler 1: Bitte geben Sie ihren Namen ein: ")
+print(player_name_1 + " spielt mit O")
+player_name_2 = input("Spieler 2:Bitte geben Sie ihren Namen ein: ")
+print(player_name_2 + " spielt mit @")
 
 print("Bitte lege dinen Stein in eine der Spalten 1, 2, 3, 4, 5, 6 oder 7")
 # Spieler wird aufgefordert den Stein in Spalte 1 - 7 zu legen
 
-zug_1 = int(input(Spielername1 + " ist am Zug"))
-if 1 <= zug_1 <= 7:
-    print("Stein legen") # Lege den Stein
+turn_1 = int(input(player_name_1 + " ist am Zug"))
+if 1 <= turn_1 <= 7:
+    print("Stein legen")  # Lege den Stein
 else:
     print("Bitte die Eingabe korrigieren und nur Zahlen zwischen 1 und 7 wählen")
-        # Schleife von vorne starten
+    # Schleife von vorne starten
 
-zug_2 = int(input(Spielername2 + " ist am Zug"))
-if 1 <= zug_2 <= 7:
-    print("Stein legen")# Lege den Stein
+turn_2 = int(input(player_name_2 + " ist am Zug"))
+if 1 <= turn_2 <= 7:
+    print("Stein legen")  # Lege den Stein
 else:
     print("Bitte die Eingabe korrigieren und nur Zahlen zwischen 1 und 7 wählen")
 
@@ -33,4 +33,3 @@ play_board = print('''
 | | | | | | | |
 | | | | | | | |
 ''')
-
