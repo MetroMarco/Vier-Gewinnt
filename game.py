0fb1011b7cfedfaefb23004c602a1023eda3b9ba
MetroMarco <gianmarco.wolf@metro.digital>
2024-09-18 15:36:41 +0200

    add playboard as a matrix and last row of the board gets filled with stones

--- game.py
+++ game.py
@@ -1,30 +1,28 @@
-STONE_1 = "0"
+STONE_1 = "O"
 STONE_2 = "@"
 board_full = False
 we_have_a_winner = False
+play_board = [[" "] * 7 for i in range(6)]
+row = 5
+column = 0
 
 
 def print_board():
-    print('''
-     1 2 3 4 5 6 7
-    | | | | | | | |
-    | | | | | | | |
-    | | | | | | | |
-    | | | | | | | |
-    | | | | | | | |
-    | | | | | | | |
-    ''')
+    for r in play_board:
+        print("|" + "|".join(r) + '|')
 
 
 def ask_players_for_turn(player_name, stone):
     while True:
-        turn = int(input(player_name + " ist am Zug"))
-        if 1 <= turn <= 7:
-            print(stone + "legen")  # Lege den Stein
+        column = int(input(player_name + " ist am Zug"))
+        if 1 <= column <= 7:
+            play_board[row][column - 1] = stone  # Lege den Stein
+            print_board()
             break
         else:
             print("Bitte die Eingabe korrigieren und nur Zahlen zwischen 1 und 7 wählen")
             # Schleife von vorne starten
+        print("\n")
 
 
 # Willkommensbildschirm
@@ -37,10 +35,7 @@ player_name_2 = input("Spieler 2:Bitte geben Sie ihren Namen ein: ")
 print(player_name_2 + " spielt mit: " + STONE_2)
 
 print("Bitte lege dinen Stein in eine der Spalten 1, 2, 3, 4, 5, 6 oder 7")
-# Spieler wird aufgefordert den Stein in Spalte 1 - 7 zu legen
 
 while not board_full and not we_have_a_winner:
     ask_players_for_turn(player_name_1, STONE_1)
-    print_board()
     ask_players_for_turn(player_name_2, STONE_2)
-    print_board()
