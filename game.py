33655b6bfba37a0ae513f32d9874eb2beccdbf15
MetroMarco <gianmarco.wolf@metro.digital>
2024-10-30 13:04:42 +0100

    add player_wins function

--- game.py
+++ game.py
@@ -22,12 +22,48 @@ def place_stone(column, stone):
             row[column_index] = stone
             break
 
-# def column_is_full(column):
-#     for row in play_board[::-1]:
-#         if column[row] == 5:
-#             print(f"{column} ist voll. Bitte wähle eine andere Spalte.")
-#             break
 
+def player_wins(stone, player_name):
+    global we_have_a_winner
+    for row in range(ROWS): # 4 x rows
+        for column in range(COLUMNS):
+            if (play_board[row][column] == stone
+                    and play_board[row][column - 1] == stone        # gibt fehler bei column 7 aus
+                    and play_board[row][column - 2] == stone
+                    and play_board[row][column - 3] == stone):
+                print(f"Player {player_name} wins")
+                we_have_a_winner = True
+                return we_have_a_winner
+
+    for row in range(ROWS): # 4 x columns
+        for column in range(COLUMNS):
+            if (play_board[row][column] == stone
+                    and play_board[row - 1][column] == stone
+                    and play_board[row - 2][column] == stone
+                    and play_board[row - 3][column] == stone):
+                print(f"Player {player_name} wins")
+                we_have_a_winner = True
+                return we_have_a_winner
+
+    for row in range(ROWS): # 4 x diagonal negativ
+        for column in range(COLUMNS):
+            if (play_board[row][column] == stone
+                    and play_board[row - 1][column - 6] == stone        # column + 1 gibt fehler bei column 7 aus
+                    and play_board[row - 2][column - 5] == stone
+                    and play_board[row - 3][column - 4] == stone):
+                print(f"Player {player_name} wins")
+                we_have_a_winner = True
+                return we_have_a_winner
+
+    for row in range(ROWS): # 4 x diagonal positiv
+        for column in range(COLUMNS):
+            if (play_board[row][column] == stone
+                    and play_board[row - 1][column - 1] == stone
+                    and play_board[row - 2][column - 2] == stone
+                    and play_board[row - 3][column - 3] == stone):
+                print(f"Player {player_name} wins")
+                we_have_a_winner = True
+                return we_have_a_winner
 
 
 def ask_players_for_turn(player_name, stone):
@@ -55,4 +91,8 @@ print("Bitte lege dinen Stein in eine der Spalten 1, 2, 3, 4, 5, 6 oder 7")
 
 while not board_full and not we_have_a_winner:
     ask_players_for_turn(player_name_1, STONE_1)
+    if player_wins(STONE_1, player_name_1):
+        break
     ask_players_for_turn(player_name_2, STONE_2)
+    if player_wins(STONE_2, player_name_2):
+        break
