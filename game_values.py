6772b405a49e7b68e6cd5f724101d070be9bec18
MetroMarco <gianmarco.wolf@metro.digital>
2025-01-22 14:37:05 +0100

    highscore format and constants

--- game_values.py
+++ game_values.py
@@ -12,8 +12,11 @@ player_name_1 = "Spieler1"
 player_name_2 = "Spieler2"
 we_have_a_winner = False
 
-highscore = {
-    "Spieler": {
-        "won": 0
-    }
-}
+HIGHSCORE_ARRAY = "highscore"
+PLAYER_DATA = "player"
+SCORE_DATA = "score"
+
+highscore = {HIGHSCORE_ARRAY: [
+    {PLAYER_DATA: "Computer",
+     SCORE_DATA: 0}
+]}
